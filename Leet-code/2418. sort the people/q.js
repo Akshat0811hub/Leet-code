21d1a1a327d3e8afcc827e890b94c64ab31736c0
Akshat0811 <singhakshat875@gmail.com>
2025-07-28 23:07:58 +0530

    error resolved

--- Leet-code/2418. sort the people/q.js
+++ Leet-code/2418. sort the people/q.js
@@ -6,7 +6,7 @@
 var sortPeople = function(names, heights) {
     let map = new Map();
     for (let i=0; i<names.length; i++){
-        map.set(heights[i] , names[i]
+        map.set(heights[i] , names[i])
     }
     heights.sort((a,b) => b-a)
 
